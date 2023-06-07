79cf5911dc40db230cd8d7b7591d3ee1965a482b
Roumaissa LEBCIR <roumaissa.lebcir@ORS.local>
2023-06-07 13:30:18 +0200

    animation

---
+++ e-powerment/lib/PlanDeCrise/Niveau01/PageNiveau01.dart
@@ -0,0 +1,324 @@
+import 'package:e_empowerment/Jouer.dart';
+
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+
+
+
+
+class pageNiveau01 extends StatefulWidget {
+  const pageNiveau01({Key? key}) : super(key: key);
+
+  @override
+  State<pageNiveau01> createState() => _pageNiveau01State();
+}
+
+class _pageNiveau01State extends State<pageNiveau01> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration:  const BoxDecoration(
+
+          image: DecorationImage(
+            image: AssetImage("images/craiyon_113930_path_on_a_desert_planet__shot_against_dive___in_vector.png"),
+            fit:BoxFit.cover,
+
+          ),
+        ),
+
+        child: Center(
+
+
+          child: Stack(
+              children:[
+
+
+
+
+                Stack(
+                    children: [
+
+                      Align(
+                        alignment: const Alignment(-0.6,0.4),
+                        child: Transform.rotate(
+                          angle: math.pi/4,
+                          child: GestureDetector(
+                            onTap: (){
+
+                            },
+                            child: Container(
+
+                                width: 93,
+                                height: 90,
+                                decoration:   const BoxDecoration(
+
+                                  image: DecorationImage(
+                                    image: AssetImage("images/etoile2.png"),
+                                    fit:BoxFit.cover,
+
+                                  ),
+
+                                  shape: BoxShape.rectangle,
+
+
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                          alignment:const Alignment(-0.5,0.37),
+                          child: Transform.rotate(
+                              angle: -math.pi/12,
+                              child: const Text('2',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
+
+                    ]
+                ),
+
+                Stack(
+                    children:[
+
+                      Align(
+                        alignment: const Alignment(0.65,0.01),
+
+                        child: Transform.rotate(
+                          angle: math.pi/120,
+                          child: GestureDetector(
+                            onTap: (){
+
+                            },
+                            child: Container(
+
+                                width: 93,
+                                height: 90,
+                                decoration:   const BoxDecoration(
+
+                                  image: DecorationImage(
+                                    image: AssetImage("images/etoile2.png"),
+                                    fit:BoxFit.cover,
+
+                                  ),
+
+                                  shape: BoxShape.rectangle,
+
+
+                                )
+                            ),
+                          ),
+                        ),
+
+                      ),
+                      Align(
+                          alignment:const Alignment(0.53,0.019),
+                          child: Transform.rotate(
+                              angle: math.pi/48,
+                              child: const Text('3',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
+
+                    ]
+                ),
+
+                Stack(
+                    children:[
+
+                      Align(
+                        alignment: const Alignment(-0.65,-0.29),
+                        child: Transform.rotate(
+                          angle: -math.pi/4,
+                          child: GestureDetector(
+                            onTap: (){
+
+                            },
+                            child: Container(
+
+                                width: 93,
+                                height: 90,
+                                decoration:   const BoxDecoration(
+
+                                  image: DecorationImage(
+                                    image: AssetImage("images/etoile2.png"),
+                                    fit:BoxFit.cover,
+
+                                  ),
+
+                                  shape: BoxShape.rectangle,
+
+
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                          alignment:const Alignment(-0.51,-0.25),
+                          child: Transform.rotate(
+                              angle: math.pi/8,
+                              child: const Text('4',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
+
+
+                    ]
+                ),
+
+                Stack(
+                    children:[
+
+                      Align(
+                        alignment: const Alignment(0.68,-0.6),
+                        child: Transform.rotate(
+                          angle: math.pi/16,
+                          child: GestureDetector(
+                            onTap: (){
+
+                            },
+                            child: Container(
+
+                                width: 93,
+                                height: 90,
+                                decoration:   const BoxDecoration(
+
+                                  image: DecorationImage(
+                                    image: AssetImage("images/etoile2.png"),
+                                    fit:BoxFit.cover,
+
+                                  ),
+
+                                  shape: BoxShape.rectangle,
+
+
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                          alignment:const Alignment(0.55,-0.53),
+                          child: Transform.rotate(
+                              angle: math.pi/12,
+                              child: const Text('5',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
+                    ]
+                ),
+
+
+                Container(
+
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    decoration:  BoxDecoration(
+
+                        color: const Color(0xff000000).withOpacity(0.4)
+
+
+
+                    )
+                ),
+
+                Stack(
+
+                  children: [
+
+
+                    Align(
+                      alignment: const Alignment(0,-0.8),
+                      child: RichText(
+                        text:  const TextSpan(
+                          text: 'Bienvenue sur la \n planète Estime de soi ',
+                          style: TextStyle(color: Colors.white , fontSize: 25 , fontWeight: FontWeight.bold),
+
+                          /*defining default style is optional */
+
+                        ),
+
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+
+                    const SizedBox(
+                        height:50 ),
+
+                    Align(
+                      alignment: const Alignment(0.65,-0.3),
+                      child: RichText(
+                        text:  const TextSpan(
+                          text: 'Commençons par le \n premier niveau',
+                          style: TextStyle(color: Colors.white , fontSize: 25 , fontWeight: FontWeight.bold),
+
+                          /*defining default style is optional */
+
+                        ),
+
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                  ],
+                ),
+
+
+
+                Stack(
+                    children: [
+
+                      Align(
+                        alignment: const Alignment(0.6,0.85),
+                        child: Transform.rotate(
+                          angle: math.pi/2,
+                          child: GestureDetector(
+                            onTap: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context){
+
+                                return Jouer();
+                                // return HiddenDrawer();
+                              },),);
+                            },
+                            child: Container(
+
+                                width: 93,
+                                height: 90,
+                                decoration:   const BoxDecoration(
+
+                                  image: DecorationImage(
+                                    image: AssetImage("images/etoile2.png"),
+                                    fit:BoxFit.cover,
+
+                                  ),
+
+                                  shape: BoxShape.rectangle,
+
+
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                          alignment:const Alignment(0.469,0.76),
+                          child: Transform.rotate(
+                              angle: math.pi/12,
+                              child: const Text('1',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),))),
+
+
+                    ]
+                ),
+
+
+
+              ]
+          ),
+
+
+
+        ),
+
+
+
+      ),
+
+    );
+  }
+}
--- e-powerment/lib/PlanDeCrise/PresentationPlanète.dart
+++ e-powerment/lib/PlanDeCrise/PresentationPlanète.dart
@@ -1,6 +1,7 @@
-import 'package:e_empowerment/Slide3.dart';
+import 'package:e_empowerment/PlanDeCrise/Niveau01/PageNiveau01.dart';
 import 'package:flutter/material.dart';
 
+import '../planète01/Niveau05/Orange/Orange1.dart';
 
 class PresentationPlanete extends StatefulWidget {
   const PresentationPlanete({Key? key}) : super(key: key);
@@ -9,14 +10,50 @@ class PresentationPlanete extends StatefulWidget {
   State<PresentationPlanete> createState() => _PresentationPlaneteState();
 }
 
-class _PresentationPlaneteState extends State<PresentationPlanete> {
+class _PresentationPlaneteState extends State<PresentationPlanete> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<Offset> _translateAnimation;
+  late Animation<Offset> _textTranslateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.5,
+    ).animate(_controller);
+
+    _translateAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: Offset(0.0, -1.5),
+    ).animate(_controller);
+
+    _textTranslateAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: Offset(0.0, 1.5), // Move text downwards
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("images/Plan.png"),
+            image: AssetImage("images/fond.png"),
             fit: BoxFit.cover,
           ),
         ),
@@ -32,39 +69,63 @@ class _PresentationPlaneteState extends State<PresentationPlanete> {
             Center(
               child: Stack(
                 children: [
-                  Align(
-                    alignment: Alignment(-0.77, -0.4),
-                    child: Container(
-                      width: 200,
-                      height: 350,
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 2, color: Colors.transparent),
-                        boxShadow: [
-                          BoxShadow(
-                            spreadRadius: 2,
-                            blurRadius: 10,
-                            color: Colors.black.withOpacity(0.1),
+                  AnimatedBuilder(
+                    animation: _controller,
+                    builder: (BuildContext context, Widget? child) {
+                      return Transform.scale(
+                        scale: _scaleAnimation.value,
+                        child: FractionalTranslation(
+                          translation: _translateAnimation.value,
+                          child: Align(
+                            alignment: Alignment(-0.77, -0.4),
+                            child: Container(
+                              width: 200,
+                              height: 350,
+                              decoration: BoxDecoration(
+                                border: Border.all(width: 2, color: Colors.transparent),
+                                boxShadow: [
+                                  BoxShadow(
+                                    spreadRadius: 2,
+                                    blurRadius: 10,
+                                    color: Colors.black.withOpacity(0.1),
+                                  ),
+                                ],
+                                shape: BoxShape.rectangle,
+                                image: const DecorationImage(
+                                  fit: BoxFit.fitHeight,
+                                  image: AssetImage("images/Poto02.png"),
+                                ),
+                              ),
+                            ),
                           ),
-                        ],
-                        shape: BoxShape.rectangle,
-                        image: const DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage("images/Poto2.png"),
                         ),
-                      ),
-                    ),
+                      );
+                    },
                   ),
-                  const Positioned(
+
+                  Positioned(
                     top: 270,
                     left: 30,
                     right: 30,
-                    child: SpeechBubble(
-                      text:
-                      'Sur cette planète, on va chercher à définir un état de crise psychique : à partir de quand tu te dis que c’est la crise, comment tu peux la prévenir et l’anticiper, qui peut t’entourer, comment la traverser...L’objectif est de profiter d’un moment où tu n’es pas en crise pour élaborer tes solutions.\n\n C’est parti !',
-                      backgroundColor: Colors.black,
-                      textColor: Colors.white,
+                    child: AnimatedBuilder(
+                      animation: _controller,
+                      builder: (BuildContext context, Widget? child) {
+                        return Transform.scale(
+                            scale: _scaleAnimation.value,
+                            child: FractionalTranslation(
+                            translation: _textTranslateAnimation.value,
+                          child: const SpeechBubble(
+                            text:
+                            'Sur cette planète, on va chercher à définir un état de crise psychique : à partir de quand tu te dis que c’est la crise, comment tu peux la prévenir et l’anticiper, qui peut t’entourer, comment la traverser...L’objectif est de profiter d’un moment où tu n’es pas en crise pour élaborer tes solutions.\n\n C’est parti !',
+                            backgroundColor: Colors.black,
+                            textColor: Colors.white,
+                          ),
+                            ) );
+                      },
+
                     ),
                   ),
+
                   Align(
                     alignment: Alignment(0, 0.99),
                     child: TextButton(
@@ -74,12 +135,13 @@ class _PresentationPlaneteState extends State<PresentationPlanete> {
                         textStyle: const TextStyle(fontSize: 20),
                       ),
                       onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) {
-                            return const Slide3();
-                          }),
-                        );
+                        setState(() {
+                          _controller.forward();
+                        });
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
+
+                          return    pageNiveau01();
+                        },),);
                       },
                       child: const Text('Taper pour passer'),
                     ),
@@ -89,47 +151,6 @@ class _PresentationPlaneteState extends State<PresentationPlanete> {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-class SpeechBubble extends StatelessWidget {
-  final String text;
-  final Color backgroundColor;
-  final Color textColor;
-
-  const SpeechBubble({
-    Key? key,
-    required this.text,
-    this.backgroundColor = Colors.white,
-    this.textColor = Colors.black,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-
-      padding: const EdgeInsets.only(top: 24,bottom: 24,right: 24,left: 24),
-      decoration: BoxDecoration(
-
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.3),
-            spreadRadius: 2,
-            blurRadius: 5,
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          Text(
-            text,
-            style: TextStyle(color: textColor, fontSize: 16),
-            textAlign: TextAlign.center,
-          ),
-        ],
       ),
     );
   }
